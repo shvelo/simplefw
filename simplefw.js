06584c3cd3219735a18de15c5c12c4160f3d32bc
Nick Shvelidze <captain@pirrate.me>
2017-03-23 01:40:15 +0400

    Separate some code

--- simplefw.js
+++ simplefw.js
@@ -5,6 +5,8 @@ const path = require('path'),
     express = require('express'),
     requireAll = require('require-all'),
     winston = require('winston'),
+    EventEmitter3 = require('eventemitter3'),
+    router = require('./lib/router'),
     fwRoot = __dirname;
 
 /**
@@ -13,17 +15,19 @@ const path = require('path'),
 const SimpleFW = {
     /**
      * Initialize SimpleFW app
+     * @name SimpleFW.init
      * @param {String} root path to app
      */
-    init: function (root) {
-        const app = express(),
-            router = express.Router();
-        let application, config, fwConfig;
+    init: function init(root) {
+        const app = express();
+
+        let application, config, fw, fwConfig;
         winston.cli();
 
         try {
             application = requireAll(path.join(root, 'application'), { recursive: true });
             config = requireAll(path.join(root, 'config'), { recursive: true });
+            fw = requireAll(path.join(fwRoot, 'fw'), { recursive: true });
             fwConfig = requireAll(path.join(fwRoot, 'config'), { recursive: true });
         } catch (exception) {
             winston.log('error', "Not a SimpleFW app:", root);
@@ -41,81 +45,23 @@ const SimpleFW = {
             config[key] = Object.assign({}, fwConfig[key], config[key] || {});
         }
 
+        // Extend with default stuff
+        for (let key in fw) {
+            if (!fw.hasOwnProperty(key))
+                continue;
+
+            application[key] = Object.assign({}, fw[key], application[key] || {});
+        }
+
+        application.root = root;
+        application.logger = winston;
+        application.events = new EventEmitter3();
+
         global.application = application;
         global.config = config;
 
-        Object.byString = function (object, string) {
-            string = string.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-            string = string.replace(/^\./, '');           // strip a leading dot
-            let a = string.split('.');
-            for (let i = 0, n = a.length; i < n; ++i) {
-                let k = a[i];
-                if (k in object) {
-                    object = object[k];
-                } else {
-                    return;
-                }
-            }
-            return object;
-        };
-
-        const registerRoutes = function (routes) {
-            for (let key in routes) {
-                if (!routes.hasOwnProperty(key))
-                    continue;
-
-                if (typeof routes[key] === 'object') {
-                    registerRoutes(routes[key]);
-                    continue;
-                }
-
-                winston.log('verbose', "Registering route:", key, routes[key]);
-
-                // Split definition: "<method> <path>"
-                let definition = key.split(' '),
-                    method = definition[0],
-                    path = definition[1],
-                    controller = Object.byString(application.controllers, routes[key]);
-
-                if (!controller) {
-                    winston.log('error', "Controller not found", routes[key]);
-                    continue;
-                }
-
-                // Support path-only routes
-                if (!path) {
-                    path = method;
-                    method = 'all';
-                }
-
-                method = method.toLowerCase();
-
-                if (http.METHODS.map(m => m.toLowerCase()).indexOf(method) === -1) {
-                    winston.log('error', "Unsupported HTTP method:", method);
-                    continue;
-                }
-
-                // Register the controller as route handler
-                router[method](path, controller);
-            }
-        };
-
-        const registerMiddleware = function (middleware) {
-            middleware.forEach(function (name) {
-                winston.log('verbose', "Registering middleware:", name);
-
-                let func = Object.byString(application.middleware, name);
-                if (!func) {
-                    winston.log('error', "Middleware not found:", name);
-                    return;
-                }
-
-                router.use(func);
-            });
-        };
-
-        registerMiddleware(config.http.middleware);
-        registerRoutes(config.routes);
+        router.registerMiddleware(config.http.middleware, application);
+        router.registerRoutes(config.routes, application);
 
         app.use(router);
         app.use(express.static(path.join(root, 'public')));
